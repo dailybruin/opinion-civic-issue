7829f579b0bad7035250bc4e30aa49add6843817
Allan Chen <allan.chen.97@hotmail.com>
2017-03-09 17:54:15 -0800

    more bugfixes

--- javascripts/timeline.js
+++ javascripts/timeline.js
@@ -1,6 +1,6 @@
 var years = ['1781','1850','1881_1','1881_2','1896','1910',/*'1911',
              '1913',*/'1915','1919','1924','1928','1929','1932','1940',
-            '1944','1946'/*,'1955'*/,'1957'/*,'1958','1960','1962'*/,'1965','1973',
+            '1944','1946'/*,'1955'*/,'1957'/*,'1958','1960','1962'*/,'1965_1','1973',
             /*'1975',*/'1984_1','1984_2','1990_1','1990_2','1992','1994',
             '2005'/*,'2006'*/,'2013'/*,'2017'*/];
 (function() {
@@ -23,13 +23,13 @@ var years = ['1781','1850','1881_1','1881_2','1896','1910',/*'1911',
 	}
 	var eventOffset = 0;
 	for(var i=1;i<=years.length;i++){
-	    if(i == years.length || $('#'+years[i]).offset()['top']-3 >= $(window).scrollTop()){
-		      $('ul.timeline li:nth-child('+(i+1)+') .leftlabel').css('background-color','#ffe301');
+	    if(i == years.length || $('#'+years[i]).offset()['top']-1 >= $(window).scrollTop()){
+		      $('ul.timeline li:nth-child('+(i)+') .leftlabel').css('background-color','#ffe301');
             if(i>years.length){
-              eventOffset=$('ul.timeline li:nth-child('+(i+1)+')').position()['top']-$(window).height()/1.5;
+              eventOffset=$('ul.timeline li:nth-child('+(i)+')').position()['top']-$(window).height()/1.5;
             }
 		        else if(i > 3)
-		          eventOffset = $('ul.timeline li:nth-child('+(i+1)+')').position()['top']-$(window).height()/2;
+		          eventOffset = $('ul.timeline li:nth-child('+(i)+')').position()['top']-$(window).height()/2;
 		      break;
 	    }
 	}
